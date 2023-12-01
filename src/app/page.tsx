749bd6ed7ff83d23776400711230cbcc3eb8b569
Jimmy Bradshaw <james.g.bradshaw@gmail.com>
2023-11-30 22:46:51 -0600

    hyphenate!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className='text-center pt-64 px-8'>
-      <p className='font-mono font-bold text-5xl'>gotahaiku4u</p>
+    <main className='text-center mt-32 mx-8'>
+      <p className='font-mono font-bold text-5xl'>got-a-haiku-4-u</p>
     </main>
   )
 }
